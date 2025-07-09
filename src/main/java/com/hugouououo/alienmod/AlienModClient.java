49235a724a753bfa9b75bae75914148fcf64777d
Hugo <hugomoreiralaprano@gmail.com>
2025-07-09 14:25:13 -0300

    1.21.2 backport (unfinished)

--- src/main/java/com/hugouououo/alienmod/AlienModClient.java
+++ src/main/java/com/hugouououo/alienmod/AlienModClient.java
@@ -1,7 +1,7 @@
 package com.hugouououo.alienmod;
 
 import com.hugouououo.alienmod.block.ModBlocks;
-import com.hugouououo.alienmod.block.entity.ModBlockEntities;
+//import com.hugouououo.alienmod.block.entity.ModBlockEntities;
 import com.hugouououo.alienmod.entity.ModEntities;
 import com.hugouououo.alienmod.entity.client.AlienModel;
 import com.hugouououo.alienmod.entity.client.AlienRenderer;
@@ -12,11 +12,10 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.data.ModelProvider;
+//import net.minecraft.client.data.ModelProvider;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
-import com.hugouououo.alienmod.block.entity.client.AlienChestModelRenderer;
-import net.minecraft.util.Identifier;
+//import com.hugouououo.alienmod.block.entity.client.AlienChestModelRenderer;
 
 public class AlienModClient implements ClientModInitializer {
     @Override
@@ -34,7 +33,7 @@ public class AlienModClient implements ClientModInitializer {
         EntityModelLayerRegistry.registerModelLayer(LaserProjectileModel.LASER_PROJECTILE, LaserProjectileModel::getTexturedModelData);
         EntityRendererRegistry.register(ModEntities.LASER_PROJECTILE, LaserProjectileRenderer::new);
 
-        // erro aqui: BlockEntityRendererFactories.register(ModBlockEntities.ALIEN_CHEST, AlienChestModelRenderer::new);
+        // BlockEntityRendererFactories.register(ModBlockEntities.ALIEN_CHEST, AlienChestModelRenderer::new);
 
     }
 
